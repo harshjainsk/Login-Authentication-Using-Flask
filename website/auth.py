21cc025b985388ae84df5017feb06148988416f0
harshjainsk <harshjainsk531@gmail.com>
2022-10-03 12:13:16 +0530

    added post and get functionality in sign-up route

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html", text = "Logging innnnn", boolean = True)
 
@@ -13,7 +13,26 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def signup():
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category = 'error')
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be of at least 7 characters')
+        else:
+            # add user to the database
+            flash('Account created!', category='success')
+
     return render_template("sign_up.html")
 
