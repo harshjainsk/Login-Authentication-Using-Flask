58fa7b821a9dbed0b050e68742d98cf14a8dfc99
harshjainsk <harshjainsk531@gmail.com>
2022-10-05 01:14:36 +0530

    removed circular import error

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
+from os import path
 
-# registering blueprints
-from .views import views
-from .auth import auth
 
 # import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
@@ -21,9 +19,30 @@ def create_app():
     # initialise the database by giving it the flask app
     db.init_app(app)
 
-    # registering blueprints with app
+    """
+    registering blueprints with app
+    """
+    # registering blueprints
+    from .views import views
+    from .auth import auth
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # import models.py file
+    from .models import User, Note
+
+    # creating database app if not created
+    create_database(app)
 
     return app
+
+
+def create_database(app):
+    """
+        If the database is not created then it will create a
+        database and store all the details in the database
+        created, else it will wrote in the existing database
+    """
+    if not path.exists('website' + DB_NAME):
+        db.create_all(app=app)
+        print("Database Created!!!")
