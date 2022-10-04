97d75d4a8dab924d4245a120974db24d95bebf8e
harshjainsk <harshjainsk531@gmail.com>
2022-10-05 01:16:54 +0530

    DateTime syntax error resolved

--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql import func
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DataTime(timezone=True), default=func.now())
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
@@ -16,5 +16,5 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
 
-    # to list out all the notes of an user
+    # to list out all the notes of a user
     notes = db.relationship('Note')
